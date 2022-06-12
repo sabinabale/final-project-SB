fab92634a28c89fb3e243b1abbd0673fd980941b
sabinabale <104379027+sabinabale@users.noreply.github.com>
2022-06-12 18:44:55 +0200

    added search function and fixed form type

--- java.js
+++ java.js
@@ -42,7 +42,20 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = `b36d8c31375cd555d60e0282d53875ba`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Prague&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = `b36d8c31375cd555d60e0282d53875ba`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#citySearch");
+  search(cityElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#searchForm");
+form.addEventListener("submit", handleSubmit);
