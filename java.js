bce36e86ba81cbdc29ef95d926456a67e77c7b06
sabinabale <104379027+sabinabale@users.noreply.github.com>
2022-06-12 17:06:32 +0200

    updated time and date

--- java.js
+++ java.js
@@ -1,3 +1,19 @@
+function displayDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
     let temperature = Math.round(response.data.main.temp);
     let tempElement = document.querySelector("#degrees");
@@ -8,12 +24,14 @@ function displayTemperature(response) {
     let humidity = response.data.main.humidity;
     let humidityElement = document.querySelector("#humidity");
     humidityElement.innerHTML = humidity;
-    let wind = response.data.wind.speed;
+    let wind = Math.round(response.data.wind.speed);
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = wind;
     let description = response.data.weather[0].description;
     let descriptionElement = document.querySelector("#description");
     descriptionElement.innerHTML = description;
+    let date = document.querySelector("#dateTime")
+    date.innerHTML = displayDate(response.data.dt * 1000);
 }
 
 let apiKey = `b36d8c31375cd555d60e0282d53875ba`;
