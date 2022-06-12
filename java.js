8ea87c3e2465b67e775f311de16ec92f6745c79c
sabinabale <104379027+sabinabale@users.noreply.github.com>
2022-06-12 16:27:23 +0200

    added display function

--- java.js
+++ java.js
@@ -1,4 +1,13 @@
-let apiKey = `b36d8c31375cd555d60e0282d53875ba`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Prague&appid=${apiKey}`;
+function displayTemperature(response) {
+    let temperature = Math.round(response.data.main.temp);
+    let tempElement = document.querySelector("#degrees");
+    tempElement.innerHTML = temperature
+    let city = response.data.name;
+    let cityElement = document.querySelector("h1");
+    cityElement.innerHTML = city;
+}
 
-console.log(apiUrl)
+let apiKey = `b36d8c31375cd555d60e0282d53875ba`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Prague&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature)
