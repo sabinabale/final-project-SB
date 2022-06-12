6dc4d15bdc2fcd1e8ca86e67890c518872d40aa5
sabinabale <104379027+sabinabale@users.noreply.github.com>
2022-06-12 16:32:49 +0200

    fixed humidity, wind and description

--- java.js
+++ java.js
@@ -5,6 +5,15 @@ function displayTemperature(response) {
     let city = response.data.name;
     let cityElement = document.querySelector("h1");
     cityElement.innerHTML = city;
+    let humidity = response.data.main.humidity;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = humidity;
+    let wind = response.data.wind.speed;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = wind;
+    let description = response.data.weather[0].description;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = description;
 }
 
 let apiKey = `b36d8c31375cd555d60e0282d53875ba`;
