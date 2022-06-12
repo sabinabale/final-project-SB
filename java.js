42086afc13cc7dbc6fe1b04d9e51cca04f2efb4f
sabinabale <104379027+sabinabale@users.noreply.github.com>
2022-06-12 18:12:47 +0200

    set up icon java

--- java.js
+++ java.js
@@ -1,40 +1,48 @@
 function displayDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-        hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-
 function displayTemperature(response) {
-    let temperature = Math.round(response.data.main.temp);
-    let tempElement = document.querySelector("#degrees");
-    tempElement.innerHTML = temperature
-    let city = response.data.name;
-    let cityElement = document.querySelector("h1");
-    cityElement.innerHTML = city;
-    let humidity = response.data.main.humidity;
-    let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = humidity;
-    let wind = Math.round(response.data.wind.speed);
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = wind;
-    let description = response.data.weather[0].description;
-    let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = description;
-    let date = document.querySelector("#dateTime")
-    date.innerHTML = displayDate(response.data.dt * 1000);
+  let tempElement = document.querySelector("#degrees");
+  let cityElement = document.querySelector("h1");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  let date = document.querySelector("#dateTime");
+  let iconElement = document.querySelector("#icon");
+
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  date.innerHTML = displayDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = `b36d8c31375cd555d60e0282d53875ba`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Prague&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature)
+axios.get(apiUrl).then(displayTemperature);
